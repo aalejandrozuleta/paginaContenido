69f94b92f780eff6138112397899aea1549b5130
aalejandrozuleta <jhoitan266@gmail.com>
2023-08-05 02:09:58 -0500

    fix "Cambio de nombre "

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom'; 
 import App from './App.jsx';
-import './public/Sass/style.scss';
+import './public/Sass/Style.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
