2be45415a7265850dee5d4886bb02ec86fd55760
aalejandrozuleta <jhoitan266@gmail.com>
2023-08-05 07:41:15 -0500

    style: cambio en home {header}

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 //import Header from "../../components/Header/Header"
-import Header from "../../components/Header/Header";
+import {Header} from "../../components/Header/Header";
 import { SliderHome } from "../../components/layouts/SliderHome/SliderHome";
 
 export const Home = () => {
